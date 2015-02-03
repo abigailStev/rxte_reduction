82de3b912e22f94ecd700c4230a58c4398907429
Abigail Stevens <A.L.Stevens@uva.nl>
2015-02-03 17:59:19 +0100

    Only writes eventlist to eventlist list if it has events in it, and checks number of cmd line args.

--- good_events.sh
+++ good_events.sh
@@ -1,20 +1,28 @@
 #! /bin/bash
 
-home_dir=$(ls -d ~)  # the -d flag is extremely important here
+## Make sure the input arguments are ok
+if (( $# != 3 )); then
+    echo -e "\t\tUsage: ./good_events.sh <prefix> <obs ID list> <GTI'd event list>"
+    exit
+fi
 
-propID=$1
+## If heainit isn't running, start it
+if (( $(echo $DYLD_LIBRARY_PATH | grep heasoft | wc -l) < 1 )); then
+	. $HEADAS/headas-init.sh
+fi
+
+prefix=$1
 obsID_list=$2
 # echo "$obsID_list"
-event_list=$3
+gtideventlist_list=$3
 
-if [ -e "$event_list" ]; then
-	rm "$event_list"
-fi
-touch "$event_list"
+home_dir=$(ls -d ~)  # the -d flag is extremely important here
+
+if [ -e "$gtideventlist_list" ]; then rm "$gtideventlist_list"; fi; touch "$gtideventlist_list"
 
 for obsID in $(cat $obsID_list); do
 # 	echo "obsID: $obsID"
-	dir="$home_dir/Reduced_data/$propID/$obsID"
+	dir="$home_dir/Reduced_data/$prefix/$obsID"
 	gtifile="$dir/gti_file.gti"
 	end_num=`ls "$dir/evt"_*.pca | wc -l`
 		
@@ -41,10 +49,18 @@ for obsID in $(cat $obsID_list); do
 			fi
 
 			if [ -e "$gtid_eventlist" ]; then
-				echo "$gtid_eventlist" >> $event_list
+				naxis2=$( python -c "from tools import get_key_val; print get_key_val('$gtid_eventlist', 1, 'NAXIS2')" )
+# 				echo "$naxis2"
+				if (( $naxis2 > 0 )); then
+					echo "$gtid_eventlist" >> $gtideventlist_list
+				else
+					echo "No good events in this event list."	
+					rm "$gtid_eventlist"
+				fi
 			fi
 		done
 	fi
 done
 
+open "$gtideventlist_list"
 echo "'Good events' script is finished."
