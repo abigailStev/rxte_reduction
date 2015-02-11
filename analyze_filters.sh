cc547c6de5ad62c471f9a25e0d23a5b7a27f8580
Abigail Stevens <A.L.Stevens@uva.nl>
2015-02-11 18:30:03 +0100

    Analyzes filter files!

--- analyze_filters.sh
+++ analyze_filters.sh
@@ -7,16 +7,14 @@
 ## 
 ###############################################################################
 
-# ## Make sure the input arguments are ok
-# if (( $# != 2 )); then
-#     echo -e "\t\tUsage: ./analyze_filters.sh <prop ID list> <filename prefix>"
-#     exit
-# fi
-# propID_list=$1
-# prefix=$2
+## Make sure the input arguments are ok
+if (( $# != 2 )); then
+    echo -e "\t\tUsage: ./analyze_filters.sh <obs ID list> <filename prefix>"
+    exit
+fi
+obslist=$1
+prefix=$2
 
-propID_list="/Users/abigailstevens/Dropbox/Lists/GX339_propIDs.lst"
-prefix="GX339-BQPO"
 
 ###############################################################################
 
@@ -34,20 +32,20 @@ if [ -e "$filter_tab" ]; then rm "$filter_tab"; fi; touch "$filter_tab"
 
 ###############################################################################
 
-for line in $( cat "$propID_list" ); do
-
-	IFS=',' read -a array <<< "$line"
-	propID="${array[1]}"
-	
-	obslist="$list_dir/${propID}_dl_obsIDs.txt"
-	if [ ! -e "$obslist" ]; then
-		obslist="$list_dir/${propID}_obsIDs.lst"
-		
-		if [ ! -e "$obslist" ]; then 
-			echo -e "\tERROR: obsID list not found for $propID."
-			continue
-		fi
-	fi
+# for line in $( cat "$propID_list" ); do
+# 
+# 	IFS=',' read -a array <<< "$line"
+# 	propID="${array[1]}"
+# 	
+# 	obslist="$list_dir/${propID}_obsIDs.lst"
+# 	if [ ! -e "$obslist" ]; then
+# 		obslist="$list_dir/${propID}_dl_obsIDs.txt"
+# 		
+# 		if [ ! -e "$obslist" ]; then 
+# 			echo -e "\tERROR: obsID list not found for $propID."
+# 			continue
+# 		fi
+# 	fi
 	
 	##########################################
 	## Loop over all PCA index files (ObsIDs)
@@ -56,12 +54,13 @@ for line in $( cat "$propID_list" ); do
 	for obsid in $( cat "$obslist" ) ; do
 		
 		if [ ! -d "$red_dir/$obsid" ]; then
-# 			echo -e "\tERROR: Reduced directory for $obsid does not exist."
+			echo -e "\tERROR: Reduced directory for $obsid does not exist."
 			continue
 		fi
 		
 		filter_file="$red_dir/$obsid"/filter.xfl
-		
+# 		echo "$filter_file"
+
 		if [ ! -e "$filter_file" ]; then
 			echo -e "\tERROR: Filter file for $obsid does not exist."
 			continue
@@ -72,13 +71,12 @@ for line in $( cat "$propID_list" ); do
 # 		fplot "$filter_file"[1] TIME "num_pcu_on pcu0_on pcu1_on pcu2_on pcu3_on pcu4_on offset elv" - /xw "hardcopy ${filter_plot}/cps"
 # 		echo "$filter_plot"
 		
-		fcalc "$filter_file"[0] temp.fits NUM_PCU_ON MIN(V)
-		
-		
-		exit
-		
+		python -c "from tools import pcu_info; pcu_info('$filter_file', '$filter_tab')"
+				
 	done
 	
-done
+# done
+
+# open "$filter_tab"
 
 ###############################################################################
