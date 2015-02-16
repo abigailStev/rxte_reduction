3221ea47c14c023c0418a2a57215d932b45e7e1b
Abigail Stevens <A.L.Stevens@uva.nl>
2015-02-16 14:17:33 +0100

    It works in a big run!

--- addpha.py
+++ addpha.py
@@ -4,24 +4,31 @@ import numpy as np
 from datetime import datetime
 import os
 
+__author__ = "Abigail Stevens"
+__author_email__ = "A.L.Stevens@uva.nl"
+__year__ = "2015"
+__description__ = "Adds multiple pha spectra together. Sums the exposure time \
+and counts. Error is summed in quadrature."
+
 """
 		addpha.py
 
-Want to add multiple pha spectra together. More than 35 at once. Don't forget
-to add exposure times as well!!
+Written in Python 2.7.
+
+Example: python addpha.py tmp_phas.txt tmp_out.pha
 
 """
 
-###############################################################################
-
+################################################################################
 if __name__ == "__main__":
 	
 	###############################
 	## Getting the input arguments
 	###############################
 	
-    parser = argparse.ArgumentParser(usage='addpha.py file_list outfile', \
-description='Adds pha spectra, in theory as many as needed.', epilog='')
+    parser = argparse.ArgumentParser(usage='addpha.py file_list outfile.pha', \
+description='Adds together multiple pha spectra. Sums the exposure time and \
+counts, error is summed in quadrature.', epilog='')
     	
     parser.add_argument('file_list', help="The full path of the (ASCII/txt/\
 dat) input file listing the spectra to be summed. One file per line.")
@@ -45,19 +52,23 @@ output file to write the summed spectra and exposure time to.")
     ###########################################################################
     ## Looping through the spectra to sum the exposure time, counts, and error
     ###########################################################################
+    
     for fits_file in infiles:
-		file_hdu = fits.open(fits_file)		
-		exposure += float(file_hdu[1].header['EXPOSURE'])
-		spectrum += file_hdu[1].data.field('COUNTS')
-		sq_error += np.square(file_hdu[1].data.field('STAT_ERR'))
-		file_hdu.close()
+    	file_hdu = fits.open(fits_file)		
+    	exposure += float(file_hdu[1].header['EXPOSURE'])
+    	spectrum += file_hdu[1].data.field('COUNTS')
+    	sq_error += np.square(file_hdu[1].data.field('STAT_ERR'))
+    	file_hdu.close()
 		
-    error = np.sqrt(sq_error) ## because adding in quadrature
+	## Done looping through fits files
+	
+    error = np.sqrt(sq_error)  ## because adding in quadrature
     
     #########################################
     ## Making FITS output (header and table)
     #########################################
     
+    ## Making FITS header (extension 0)
     prihdr = fits.Header()
     prihdr.set('TYPE', "Summed energy spectra.")
     prihdr.set('DATE', str(datetime.now()), "YYYY-MM-DD localtime")
@@ -65,7 +76,7 @@ output file to write the summed spectra and exposure time to.")
     prihdr.set('EXPOSURE', exposure, "seconds")
     prihdu = fits.PrimaryHDU(header=prihdr)
     
-    ## Making FITS table
+    ## Making FITS table (extension 1)
     col1 = fits.Column(name='CHANNEL', format='I', array=channels)
     col2 = fits.Column(name='COUNTS', unit='count', format='D', \
     	array=spectrum)
@@ -74,13 +85,14 @@ output file to write the summed spectra and exposure time to.")
     cols = fits.ColDefs([col1, col2, col3])
     tbhdu = fits.BinTableHDU.from_columns(cols)
     
-    ## If the file already exists, remove it (still working on just updating it)
+    ## If the file already exists, remove it
     if os.path.isfile(args.out_file):
     	os.remove(args.out_file)
+    
     ## Writing to a FITS file
     thdulist = fits.HDUList([prihdu, tbhdu])
     thdulist.writeto(args.out_file)	
 
 ## End of program 'addpha.py'
 
-###############################################################################
+################################################################################
