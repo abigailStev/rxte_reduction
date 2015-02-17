418273005308fd919d2f068dbc7551ab61062b10
Abigail Stevens <A.L.Stevens@uva.nl>
2015-02-17 18:59:45 +0100

    gives argument to pcu_filter.py.

--- analyze_filters.sh
+++ analyze_filters.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-###############################################################################
-##
-##
-##
+################################################################################
 ## 
-###############################################################################
+## Analyzes filter files from multiple obsIDs to show which pcus are on when.
+## 
+## Written by Abigail Stevens, A.L.Stevens@uva.nl, 2015
+## 
+################################################################################
 
 ## Make sure the input arguments are ok
 # if (( $# != 2 )); then
-#     echo -e "\t\tUsage: ./analyze_filters.sh <obs ID list> <filename prefix>"
+#     echo -e "\t\tUsage: ./analyze_filters.sh <obs ID list> <filename prefix>\n"
 #     exit
 # fi
 # obslist=$1
@@ -18,7 +19,7 @@
 obslist="/Users/abigailstevens/Dropbox/Lists/GX339-BQPO_obsIDs.lst" 
 prefix="GX339-BQPO"
 
-###############################################################################
+################################################################################
 
 ## If heainit isn't running, start it
 if (( $(echo $DYLD_LIBRARY_PATH | grep heasoft | wc -l) < 1 )); then
@@ -27,14 +28,15 @@ fi
 
 home_dir=$(ls -d ~)  ## The home directory of this machine
 list_dir="$home_dir/Dropbox/Lists"
-red_dir="$home_dir/Reduced_data/${prefix}"
+# red_dir="$home_dir/Reduced_data/${prefix}"
+red_dir="$home_dir/Dropbox/Research/sample_data"
 script_dir="$home_dir/Dropbox/Research/rxte_reduce"
 filter_tab="$red_dir/filters.dat"
 if [ -e "$filter_tab" ]; then rm "$filter_tab"; fi; touch "$filter_tab"
 filter_list="${red_dir}/tmp_filters.txt"
 if [ -e "$filter_list" ]; then rm "$filter_list"; fi; touch "$filter_list"
 
-###############################################################################
+################################################################################
 
 # for line in $( cat "$propID_list" ); do
 # 
@@ -57,12 +59,13 @@ if [ -e "$filter_list" ]; then rm "$filter_list"; fi; touch "$filter_list"
 	
 	for obsid in $( cat "$obslist" ) ; do
 		
-		if [ ! -d "$red_dir/$obsid" ]; then
-			echo -e "\tERROR: Reduced directory for $obsid does not exist."
-			continue
-		fi
-		
-		filter_file="$red_dir/$obsid"/filter.xfl
+		# if [ ! -d "$red_dir/$obsid" ]; then
+# 			echo -e "\tERROR: Reduced directory for $obsid does not exist."
+# 			continue
+# 		fi
+# 		
+# 		filter_file="$red_dir/$obsid"/filter.xfl
+		filter_file="$home_dir/Dropbox/Research/sample_data/${obsid}_filter.xfl"
 # 		echo "$filter_file"
 
 		if [ ! -e "$filter_file" ]; then
@@ -76,9 +79,9 @@ if [ -e "$filter_list" ]; then rm "$filter_list"; fi; touch "$filter_list"
 	
 # done
 
-python "$script_dir"/pcu_filter.py
+python "$script_dir"/pcu_filter.py "$filter_list"
 		
 
 # open "$filter_tab"
 
-###############################################################################
+################################################################################
