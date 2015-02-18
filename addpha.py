5bdbb4e44652697d6e510fd6a73eaea72e19b721
Abigail Stevens <A.L.Stevens@uva.nl>
2015-02-18 15:39:24 +0100

    Elaborated in top block comment, now it can do any number of energy bins (as long as theyre all the same) because it gets the length from DETCHANS.

--- addpha.py
+++ addpha.py
@@ -19,6 +19,9 @@ and counts. Error is summed in quadrature."
 Written in Python 2.7.
 
 Example call: python addpha.py tmp_phas.txt ./tmp_out.pha tmp_all.gti
+	where tmp_phas.txt has a list of the pha files to be added
+		  tmp_out.pha is the name of the output spectrum
+		  tmp_all.gti is the gti used on the individual pha files
 
 """
 
@@ -29,9 +32,10 @@ if __name__ == "__main__":
 	## Getting the input arguments
 	###############################
 	
-    parser = argparse.ArgumentParser(usage='addpha.py file_list outfile.pha', \
-description='Adds together multiple pha spectra. Sums the exposure time and \
-counts, error is summed in quadrature.', epilog='')
+    parser = argparse.ArgumentParser(usage='addpha.py file_list outfile.pha \
+gti_file.gti', description='Adds together multiple pha spectra. Sums the \
+exposure time and counts, error is summed in quadrature.', epilog='All \
+arguments are required.')
     	
     parser.add_argument('file_list', help="The full path of the (ASCII/txt/\
 dat) input file listing the spectra to be summed. One file per line.")
@@ -48,13 +52,21 @@ added.")
     ## Initializing variables
     ##########################
     
+    ## Make list of file names from the list
     infiles = [line.strip() for line in open(args.file_list)]
     
+    ## Need to copy the first file and write over it -- this way it has all the
+    ## header information needed by other FTOOLS. Relevant keywords are also 
+    ## overwritten below.
     subprocess.call(["cp", infiles[0], args.out_file])
+    
+    ## Get number of detector energy channels
+    detchans = tools.get_key_val(infiles[0], 1, "DETCHANS")  
+    
     exposure = 0.0
-    spectrum = np.zeros(256)
-    sq_error = np.zeros(256)
-    channels = np.arange(256)
+    spectrum = np.zeros(detchans)
+    sq_error = np.zeros(detchans)
+    channels = np.arange(detchans)
     
     ###########################################################################
     ## Looping through the spectra to sum the exposure time, counts, and error
@@ -106,18 +118,21 @@ added.")
     hdr0.set('TSTOP', tstop)
     hdr0.set('DATE-END', dateend)
     hdr0.set('TIME-END', timeend)
+    hdr0.set('CREATOR', "addpha.py")
     hdr1 = out_hdu[1].header
     hdr1.set('TSTOP', tstop)
     hdr1.set('DATE-END', dateend)
     hdr1.set('TIME-END', timeend)
     hdr1.set('EXPOSURE', exposure)
     hdr1.set('FILEN1', args.file_list)
+    hdr1.set('CREATOR', "addpha.py")
     hdr2 = out_hdu[2].header
     hdr2.set('TSTOPI', tstopi)
     hdr2.set('TSTOPF', tstopf)
     hdr2.set('DATE-END', dateend)
     hdr2.set('TIME-END', timeend)
     hdr2.set('ONTIME', exposure)
+    hdr2.set('CREATOR', "addpha.py")
 	
 	## Saving the changes
     out_hdu.flush()
