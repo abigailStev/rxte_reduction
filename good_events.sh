7d6404a9b5dee45be7484b8dc63f55cbb1de5310
Abigail Stevens <A.L.Stevens@uva.nl>
2015-02-23 11:06:06 +0100

    Removed an exit statement from previous tests.

--- good_events.sh
+++ good_events.sh
@@ -11,6 +11,7 @@
 ########################################
 ## Make sure the input arguments are ok
 ########################################
+
 if (( $# != 3 )); then
     echo -e "\t\tUsage: ./good_events.sh <prefix> <obs ID list> <list of GTI'd event lists to write to>\n"
     exit
@@ -20,9 +21,12 @@ prefix=$1
 obsID_list=$2
 gtideventlist_list=$3
 
+################################################################################
+
 ######################################
 ## If heainit isn't running, start it
 ######################################
+
 if (( $(echo $DYLD_LIBRARY_PATH | grep heasoft | wc -l) < 1 )); then
 	. $HEADAS/headas-init.sh
 fi
@@ -34,6 +38,9 @@ red_dir="$home_dir/Reduced_data/$prefix"
 
 if [ -e "$gtideventlist_list" ]; then rm "$gtideventlist_list"; fi; touch "$gtideventlist_list"
 
+################################################################################
+################################################################################
+
 ############################################
 ## Looping through the obsIDs in obsID_list
 ############################################
@@ -41,23 +48,28 @@ for obsID in $( cat "$obsID_list" ); do
 
 	data_dir="$red_dir/$obsID"
 	gtifile="$data_dir/gti_file.gti"
-	num_files=`ls "$data_dir/evt"_*.pca | wc -l`
-		
-# 	echo "Number of files for this obsID: $num_files"
+	num_files=$( ls "$data_dir/evt"_*.pca | wc -l )
+	
+	####################################################
+	## If there are event files to decode in this obsID
+	####################################################
+	
 	if (( num_files>0 )); then
+		
+		#######################################################
+		## Looping through the different event files per obsID
+		#######################################################
+		
 		for (( num=1;num<=num_files;++num )); do
 		
 			binaryfile="$data_dir/evt_${num}.pca"
 			eventlist="$data_dir/eventlist_${num}.fits"
-			gtid_eventlist="$data_dir/GTId_eventlist_${num}.fits"
-# 			gtid_eventlist="$data_dir/GTId_eventlist_${num}.dat"
-			
 			if [ -e "$eventlist" ]; then rm "$eventlist"; fi
-			
+		
 			##################################
 			## 'Decode' the binary event list
 			##################################
-			
+		
 			if [ -e "$binaryfile" ]; then
 				decodeevt infile="$binaryfile" outfile="$eventlist" > dump.txt
 			else
@@ -65,31 +77,59 @@ for obsID in $( cat "$obsID_list" ); do
 				continue
 			fi
 			
-			####################
-			## Run apply_gti.py
-			####################
-			
-			if [ -e "$eventlist" ] && [ -e "$gtifile" ]; then
-				python "$exe_dir/apply_gti.py" "$eventlist" "$gtifile" "$gtid_eventlist"
-			else
-				continue
-			fi
-
-			if [ -e "$gtid_eventlist" ]; then
-				naxis2=$( python -c "from tools import get_key_val; print get_key_val('$gtid_eventlist', 1, 'NAXIS2')" )
-				
-				## Only append the GTI'd eventlist file name to the list if it 
-				## has good events in it (i.e. length > 0)
-				if (( $naxis2 > 0 )); then
-					echo "$gtid_eventlist" >> $gtideventlist_list
-				else
-					echo "No good events in this event list."	
-					rm "$gtid_eventlist"
-				fi  ## End of 'if there are good events in this eventlist'
-				
-			fi  ## End of 'if gti'd eventlist exists, i.e. apply_gti worked.
-			
 		done  ## End of looping through the eventlists in this obsID
+		
+		##################################################################
+		## Merging multiple orbits from one obsID into a single eventlist
+		##################################################################
+		
+		merged_eventlist="$data_dir/eventlist.fits"
+		temp_list="${data_dir}/multi_evts.lst"
+		
+		if (( num_files > 1 )); then
+			ls "$data_dir"/eventlist_*.fits > $temp_list
+			fmerge infiles=@"$temp_list" \
+				outfile="$merged_eventlist" \
+				columns=- \
+				copyprime=yes \
+				lastkey='TSTOP DATE-END TIME-END'  \
+				clobber=yes
+		else  ## If there's only one file, just copy it
+			cp "${eventlist}" "${merged_eventlist}"
+		fi
+		echo "$merged_eventlist"
+		gtid_eventlist="$data_dir/GTId_eventlist.fits"
+	# 	gtid_eventlist="$data_dir/GTId_eventlist.dat"
+	
+		####################
+		## Run apply_gti.py
+		####################
+		
+		if [ -e "$eventlist" ] && [ -e "$gtifile" ]; then
+			python "$exe_dir/apply_gti.py" "$merged_eventlist" "$gtifile" "$gtid_eventlist"
+		else
+			continue
+		fi
+
+		if [ -e "$gtid_eventlist" ]; then
+			naxis2=$( python -c "from tools import get_key_val; print get_key_val('$gtid_eventlist', 1, 'NAXIS2')" )
+			
+			###############################################################
+			## Only append the GTI'd eventlist file name to the list if it 
+			## has good events in it (i.e. length > 0)
+			###############################################################
+			
+			if (( $naxis2 > 0 )); then
+				echo "$gtid_eventlist" >> $gtideventlist_list
+			else
+				echo -e "\tNo good events in this event list."	
+				rm "$gtid_eventlist"
+			fi  ## End of 'if there are good events in this eventlist'
+		else
+			echo -e "\tERROR: apply_gti.py did not work. GTI'd eventlist does not exist."
+			continue
+		fi  ## End of 'if gti'd eventlist exists, i.e. apply_gti worked.
+		
 	fi  ## End of 'if there are event files in this obsID directory'
 	
 done  ## End of looping through obsIDs in obsID_list
