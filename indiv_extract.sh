2f5443865d0a713bfba2335e28f77adae35105d2
Abigail Stevens <A.L.Stevens@uva.nl>
2015-02-23 20:14:21 +0100

    Improved top header comment and spacing, commented things out.

--- indiv_extract.sh
+++ indiv_extract.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-#######################
+################################################################################
 ## 
+## Extracts light curves and spectra for individual obsIDs, for Standard-2, 
+## Standard-1, and event-mode data.
+##
+## Example call: ./indiv_extract.sh /out/put/dir progress.log
+##
+## Change the directory names and specifiers before the double '#' row to best
+## suit your setup.
+##
+## Notes: HEASOFT 6.11.*, bash 3.*, and Python 2.7.* (with supporting libraries) 
+## 		  must be installed in order to run this script. 
+##
+## Written by Abigail Stevens, A.L.Stevens@uva.nl, 2015
 ## 
-## 
-## 
-#######################
-
 ################################################################################
 
-########################################
 ## Make sure the input arguments are ok
-########################################
-
 if (( $# != 2 )); then
     echo -e "\t\tUsage: ./indiv_extract.sh <output dir> <progress log>\n"
     exit
@@ -23,6 +28,11 @@ progress_log=$2
 
 ################################################################################
 
+## If heainit isn't running, start it
+if (( $(echo $DYLD_LIBRARY_PATH | grep heasoft | wc -l) < 1 )); then
+	. $HEADAS/headas-init.sh
+fi
+
 home_dir=$(ls -d ~) 
 list_dir="$home_dir/Dropbox/Lists"
 gti_file="$out_dir/gti_file.gti"
@@ -30,54 +40,55 @@ gti_file="$out_dir/gti_file.gti"
 sa_cols="$out_dir/std2_cols.pcu"
 cat "$list_dir"/std2_pcu2_cols.lst > "$sa_cols"
 
+################################################################################
+################################################################################
+
 ##############################
 ## Extracting Standard-2 data
 ##############################
 
-if [ -e "$out_dir/std2.lst" ]; then rm -f "$out_dir/std2.lst"; fi
-ls $out_dir/std2*.pca > "$out_dir/std2.lst"
-echo "Extracting Std2 data" >> $progress_log
-
-saextrct lcbinarray=10000000 \
-	maxmiss=200 \
-	infile=@"$out_dir/std2.lst" \
-	gtiorfile=- \
-	gtiandfile="$gti_file" \
-	outroot="$out_dir/std2" \
-	columns=@"$sa_cols" \
-	accumulate=ONE \
-	timecol=TIME \
-	binsz=16 \
-	mfracexp=INDEF \
-	printmode=BOTH \
-	lcmode=RATE \
-	spmode=SUM \
-	mlcinten=INDEF \
-	mspinten=INDEF \
-	writesum=- \
-	writemean=- \
-	timemin=INDEF \
-	timemax=INDEF \
-	timeint=INDEF \
-	chmin=INDEF \
-	chmax=INDEF \
-	chint=INDEF \
-	chbin=INDEF \
-	dryrun=no \
-	clobber=yes
-
-if [ -e $dump_file ]; then rm -f $dump_file; fi
-
-if [ ! -e "$out_dir/std2.pha" ]; then
-	echo -e "\tERROR: Std2 spectrum not made!"
-	echo -e "\tERROR: Std2 spectrum not made!" >> $progress_log
-# 	continue
-fi 
-if [ ! -e "$out_dir/std2.lc" ]; then
-	echo -e "\tERROR: Std2 light curve not made!"
-	echo -e "\tERROR: Std2 light curve not made!" >> $progress_log
-# 	continue
-fi
+# if [ -e "$out_dir/std2.lst" ]; then rm -f "$out_dir/std2.lst"; fi
+# ls $out_dir/std2*.pca > "$out_dir/std2.lst"
+# echo "Extracting Std2 data" >> $progress_log
+# 
+# saextrct lcbinarray=10000000 \
+# 	maxmiss=200 \
+# 	infile=@"$out_dir/std2.lst" \
+# 	gtiorfile=- \
+# 	gtiandfile="$gti_file" \
+# 	outroot="$out_dir/std2" \
+# 	columns=@"$sa_cols" \
+# 	accumulate=ONE \
+# 	timecol=TIME \
+# 	binsz=16 \
+# 	mfracexp=INDEF \
+# 	printmode=BOTH \
+# 	lcmode=RATE \
+# 	spmode=SUM \
+# 	mlcinten=INDEF \
+# 	mspinten=INDEF \
+# 	writesum=- \
+# 	writemean=- \
+# 	timemin=INDEF \
+# 	timemax=INDEF \
+# 	timeint=INDEF \
+# 	chmin=INDEF \
+# 	chmax=INDEF \
+# 	chint=INDEF \
+# 	chbin=INDEF \
+# 	dryrun=no \
+# 	clobber=yes
+# 
+# if [ ! -e "$out_dir/std2.pha" ]; then
+# 	echo -e "\tERROR: Std2 spectrum not made!"
+# 	echo -e "\tERROR: Std2 spectrum not made!" >> $progress_log
+# # 	continue
+# fi 
+# if [ ! -e "$out_dir/std2.lc" ]; then
+# 	echo -e "\tERROR: Std2 light curve not made!"
+# 	echo -e "\tERROR: Std2 light curve not made!" >> $progress_log
+# # 	continue
+# fi
 
 # ls $out_dir/vle*.pca > "$out_dir/vle.lst"
 # ## Get the VLE rates from std1b files
@@ -110,8 +121,6 @@ fi
 # 	dryrun=no \
 # 	clobber=yes
 # 
-# if [ -e $dump_file ] ; then rm -f $dump_file; fi
-
 echo "SAEXTRCT finished"
 
 ################################################################################
